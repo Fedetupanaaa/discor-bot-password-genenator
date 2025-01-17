a17883c90e108a9b393fdfd4a5d1974d4f5f6bf3
federico <ficomaubrigades4@gmail.com>
2025-01-17 20:31:09 -0300

    Create bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,10 @@
+import random
+
+def gen_pass(pass_length):
+    caracter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(caracter)
+
+    return password
