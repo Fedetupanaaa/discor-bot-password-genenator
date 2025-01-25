1237a5ae61158e0f155b646ce17d059e2508ab77
federico <ficomaubrigades4@gmail.com>
2025-01-24 21:31:21 -0300

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -8,3 +8,15 @@ def gen_pass(pass_length):
         password += random.choice(caracter)
 
     return password
+
+def gen_emoji():
+    emoji = ["💀", "😎","😵‍💫" ,"🥳", "🤡","🤑","😰","😮","❤️"]
+    return random.choice(emoji)
+
+
+def coin_flip():
+    giro = random.randint(0, 2)
+    if giro == 0:
+        return "Cara"
+    else:
+        return "cruz"
