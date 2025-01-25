6c48a60101ef95a09cf1df3013eff4d2b1490cd3
federico <ficomaubrigades4@gmail.com>
2025-01-24 21:30:56 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
 from bot_logic import gen_pass
+from bot_logic import gen_emoji
+from bot_logic import coin_flip
+import random
 from discord.ext import commands
+import time
 
 
 intents = discord.Intents.default()
@@ -20,6 +24,23 @@ async def gen(ctx):
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def emoji(ctx):
+    await ctx.send(f" {gen_emoji()}")
+
+@bot.command()
+async def numero(ctx):
+    numero_aleatorio = random.randint(1, 10)
+    await ctx.send(f'El número aleatorio es: {numero_aleatorio}')
+    
+
+@bot.command()
+async def coinflip(ctx):
+    await ctx.send(f"la moneda dice: {coin_flip()}")    
+
+
+
 
 
 
